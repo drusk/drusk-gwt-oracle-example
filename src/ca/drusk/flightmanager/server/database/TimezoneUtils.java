a9f0f2854e9a455aad1c2836c3e6498da7b19724
David Rusk <drusk@uvic.ca>
2012-09-09 14:26:17 -0700

    Removed empty test class, added javadoc

--- src/ca/drusk/flightmanager/server/database/TimezoneUtils.java
+++ src/ca/drusk/flightmanager/server/database/TimezoneUtils.java
@@ -1,5 +1,11 @@
 package ca.drusk.flightmanager.server.database;
 
+/**
+ * Contains constants used for working with Oracle timezons.
+ * 
+ * @author drusk
+ * 
+ */
 public class TimezoneUtils {
 
 	public static final String TO_TIMESTAMP_TZ = "TO_TIMESTAMP_TZ(?, 'MON DD, YYYY HH24:MI TZH:TZM')";
